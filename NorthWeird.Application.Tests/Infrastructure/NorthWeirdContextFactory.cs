22a307d35e4082c6fbb762ea54cc5324d6b6cc34
Katsiaryna Kuzkina <katsiaryna_kuzkina@epam.com>
2019-03-30 21:38:44 +0300

    Add DB mock

---
+++ NorthWeird.Application.Tests/Infrastructure/NorthWeirdContextFactory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWeird.Persistence;
+using System;
+using System.Collections.Generic;
+using NorthWeird.Domain.Entities;
+
+namespace NorthWeird.Application.Tests.Infrastructure
+{
+    public class NorthWeirdContextFactory
+    {
+        public static NorthWeirdDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<NorthWeirdDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new NorthWeirdDbContext(options);
+
+            context.Database.EnsureCreated();
+
+            context.Suppliers.AddRange(new List<Supplier>
+            {
+                new Supplier { SupplierId = 1, CompanyName = "Dumpling boutique" },
+                new Supplier { SupplierId = 2, CompanyName = "Washington Bread Factory №2" },
+                new Supplier { SupplierId = 3, CompanyName = "Alaska fishing inс" }
+            });
+
+            context.Categories.AddRange(new List<Category>
+            {
+                new Category { CategoryId = 1, CategoryName = "Fish" },
+                new Category { CategoryId = 2, CategoryName = "Meat" },
+                new Category { CategoryId = 3, CategoryName = "Vegetables" }
+            });
+
+            context.Products.AddRange(new List<Product>
+            {
+                new Product { ProductId = 1, ProductName = "Frutella", CategoryId = 1, SupplierId = 1 },
+                new Product { ProductId = 2, ProductName = "Mars", CategoryId = 2, SupplierId = 2 },
+                new Product { ProductId = 3, ProductName = "Onion", CategoryId = 3, SupplierId = 3 }
+            });
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(NorthWeirdDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
---
+++ NorthWeird.Application.Tests/Services/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NorthWeird.Application.Tests.Services
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
