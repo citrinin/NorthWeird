610fc6011c97c9aa2fa6e63286955a7eeeb18104
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2019-04-07 23:48:37 +0300

    Add breadcrumb

---
+++ NorthWeird.WebUI/Components/Breadcrumb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NorthWeird.WebUI.Components
+{
+    public class Breadcrumb : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewData["controllerName"] = this.ViewContext.RouteData.Values["controller"].ToString();
+            ViewData["actionName"] = this.ViewContext.RouteData.Values["action"].ToString();
+            return View();
+        }
+    }
+}
