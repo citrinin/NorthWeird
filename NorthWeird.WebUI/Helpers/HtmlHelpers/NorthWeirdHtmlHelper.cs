157000f4764bda9d907b1cff2023fb2bacea9731
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2019-04-08 01:06:24 +0300

    Add htmlHelper and tagHelper

---
+++ NorthWeird.WebUI/Helpers/HtmlHelpers/NorthWeirdHtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NorthWeird.WebUI.Helpers.HtmlHelpers
+{
+    public static class NorthWeirdHtmlHelper
+    {
+        public static IHtmlContent NorthWeirdImageLink(this IHtmlHelper htmlHelper,
+            int imageId, string linkText)
+        {
+            return htmlHelper.RouteLink(linkText, "Image", new {id = imageId});
+        }
+    }
+}
---
+++ NorthWeird.WebUI/Helpers/TagHelpers/NorthWeirdImageTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NorthWeird.WebUI.Helpers.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northweird-id")]
+    public class NorthWeirdImageTagHelper : TagHelper
+    {
+        public int NorthweirdId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"/image/{NorthweirdId}");
+        }
+    }
+}
