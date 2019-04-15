c9f3e73a2e88fc4f7fdf583ecfca6134cf5f3e94
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2019-04-15 09:12:19 +0300

    Add api project

---
+++ NorthWeird.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NorthWeird.WebApi.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Kate", Surname = "Kuzkina" });
+        }
+    }
+}
---
+++ NorthWeird.WebApi/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthWeird.Application.Interfaces;
+using NorthWeird.Application.Services;
+using NorthWeird.Persistence;
+
+namespace NorthWeird.WebApi
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<NorthWeirdDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("Northwind")));
+
+            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<ICategoryData, SqlCategoryData>();
+            services.AddScoped<ISupplierData, SqlSupplierData>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc(config =>
+            {
+                config.MapRoute("NorthWeird", "api/{controller}/{action}");
+            });
+        }
+    }
+}
