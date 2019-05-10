d23b5b5fd87358fd454d348b843e2133e5a86e78
Katsiaryna Kuzkina <katsiaryna_kuzkina@epam.com>
2019-05-10 18:33:12 +0300

    Add dto models

---
+++ NorthWeird.Application/Models/CategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWeird.Application.Models
+{
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Picture { get; set; }
+    }
+}
---
+++ NorthWeird.Application/Models/ProductDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWeird.Application.Models
+{
+    public class ProductDto
+    {
+        public string ProductName { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public short UnitsInStock { get; set; }
+
+        public short UnitsOnOrder { get; set; }
+
+        public short ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string SupplierName { get; set; }
+    }
+}
