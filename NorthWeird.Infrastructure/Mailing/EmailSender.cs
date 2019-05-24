9036a3c0b088ebb15eaf75538a0cf5a90fd986ba
Katsiaryna Kuzkina <katsiaryna_kuzkina@epam.com>
2019-05-24 20:34:37 +0300

    Move cridentials to the appsettings.json

--- NorthWeird.Infrastructure/Mailing/EmailSender.cs
+++ NorthWeird.Infrastructure/Mailing/EmailSender.cs
@@ -2,14 +2,23 @@
 using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace NorthWeird.Infrastructure.Mailing
 {
     public class EmailSender: IEmailSender
     {
-        private const string FromEmail = "nortweird@gmail.com";
-        private const string SmtpServer = "smtp.gmail.com";
-        private const string Password = "Ghbdtn1!";
+
+        private readonly string _fromEmail;
+        private readonly string _smtpServer;
+        private readonly string _password;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _fromEmail = configuration.GetValue<string>("EmailSender:Email");
+            _password = configuration.GetValue<string>("EmailSender:Password");
+            _smtpServer = configuration.GetValue<string>("EmailSender:SmtpServer");
+        }
 
         public async Task SendEmailAsync(string emailAddress, string subject, string message)
         {
@@ -17,7 +26,7 @@ namespace NorthWeird.Infrastructure.Mailing
             {
                 var mailMessage = new MailMessage
                 {
-                    From = new MailAddress(FromEmail),
+                    From = new MailAddress(_fromEmail),
                     To = { emailAddress },
                     Subject = subject,
                     Body = message,
@@ -29,12 +38,12 @@ namespace NorthWeird.Infrastructure.Mailing
             }
         }
 
-        private static SmtpClient GetClient()
+        private SmtpClient GetClient()
         {
-            return new SmtpClient(SmtpServer)
+            return new SmtpClient(_smtpServer)
             {
                 UseDefaultCredentials = true,
-                Credentials = new NetworkCredential(FromEmail, Password),
+                Credentials = new NetworkCredential(_fromEmail, _password),
                 Port = 587,
                 EnableSsl = true
             };
